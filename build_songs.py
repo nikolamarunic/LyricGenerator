cf71fabba3d781cdf893c0945ecf9db938c3e647
Nikola Marunic <nikola.marunic@mail.utoronto.ca>
2019-11-04 15:53:18 -0500

    Created function get_new_word for building the song.

--- build_songs.py
+++ build_songs.py
@@ -1,34 +1,4 @@
 from random import choice
-# def generatemodel(text, order):
-#     model = {}
-#     for i in range(0, len(text) - order):
-#         fragment = text[i:i + order]
-#         next_letter = text[i + order]
-#         if fragment not in model:
-#             model[fragment] = {}
-#         if next_letter not in model[fragment]:
-#             model[fragment][next_letter] = 1
-#         else:
-#             model[fragment][next_letter] += 1
-#     return model
-#
-#
-# print(generatemodel("bobby", 1))
-
-def generate_random_song(lyrics: str, context_length: int, num_words:int) -> str:
-    """Returns a randomly generated song with num_words words based on a context
-    of context_length words from the text in lyrics.
-    :param lyrics: Lyrics of the original song
-    :param context_length: the length of the context to build from.
-    :param num_words: Word length of the new song.
-    :return: the new song.
-    """
-    song = ''
-    wordlist = lyrics.split(' ')
-    context_to_words = build_context_to_next_words(wordlist, context_length)
-#    song = generate_song(context_to_words, num_words)
-    return song
-
 #def build_context_to_next_words(wordlist: list, context_length: int) -> dict[tuple[str], list[str]]:
 def build_context_to_next_words(wordlist: list, context_length: int) -> dict:
     """
@@ -44,9 +14,51 @@ def build_context_to_next_words(wordlist: list, context_length: int) -> dict:
         else:
             context_dict[context].append(wordlist[i + context_length])
     return context_dict
-
-# def generate_song(context_to_words: dict[tuple[str], list[str]] , num_words: int) -> str:
-#     pass
-
 # lst = ["Hey", "my", "life", "is", "niko", "and", "my", "life", "is", "sick"]
 # print (build_context_to_next_words(lst, 2))
+
+
+def generate_random_song(lyrics: str, context_length: int, num_words:int) -> str:
+    """Returns a randomly generated song with num_words words based on a context
+    of context_length words from the text in lyrics.
+    :param lyrics: Lyrics of the original song
+    :param context_length: the length of the context to build from.
+    :param num_words: Word length of the new song.
+    :return: the new song.
+    """
+    song = ''
+    wordlist = lyrics.split(' ')
+    context_to_words = build_context_to_next_words(wordlist, context_length)
+    #print(context_to_words)
+    current_context = wordlist[1: 1 +context_length]
+#    song = generate_song(context_to_words, num_words)
+
+    for i in range(0, num_words - context_length):
+        new_word = get_new_word(context_to_words, tuple(current_context))
+
+    return song
+
+def get_new_word(model: dict, fragment: tuple) -> str:
+    words = []
+    for word in model[fragment]:
+        words.append(word)
+    #print(words)
+    return choice(words)
+
+lyrics = "Hey my life is niko and my life is sick"
+generate_random_song(lyrics, 2, 10)
+# lst = ["Hey", "my", "life", "is", "niko", "and", "my", "life", "is", "sick"]
+# print (build_context_to_next_words(lst, 2))
+
+
+# def generate_song(model: dict , num_words: int) -> str:
+#     new_song = ""
+#     current_context = ""
+#     for i in range (num_words):
+#
+#
+#     return new_song
+#
+
+
+
