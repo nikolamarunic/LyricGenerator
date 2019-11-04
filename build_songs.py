18237a797293605876478d4865e71b53a456cbd3
Nikola Marunic <nikola.marunic@mail.utoronto.ca>
2019-11-04 15:23:14 -0500

    Created the function that builds the context to the next word.

--- build_songs.py
+++ build_songs.py
@@ -1,6 +1,4 @@
 from random import choice
-
-
 # def generatemodel(text, order):
 #     model = {}
 #     for i in range(0, len(text) - order):
@@ -26,12 +24,29 @@ def generate_random_song(lyrics: str, context_length: int, num_words:int) -> str
     :return: the new song.
     """
     song = ''
-    wordlist = []
+    wordlist = lyrics.split(' ')
     context_to_words = build_context_to_next_words(wordlist, context_length)
-    song = generate_song(context_to_words, num_words)
+#    song = generate_song(context_to_words, num_words)
+    return song
 
-def build_context_to_next_words(wordlist: list[str], context_length: int) -> dict[tuple[str], List[str]]:
-    pass
+#def build_context_to_next_words(wordlist: list, context_length: int) -> dict[tuple[str], list[str]]:
+def build_context_to_next_words(wordlist: list, context_length: int) -> dict:
+    """
+    :param wordlist: The given list of words.
+    :param context_length:  The given length of a context.
+    :return: the dictionary with all the contexts and possible next words.
+    """
+    context_dict = {}
+    for i in range(len(wordlist) - context_length):
+        context = tuple(wordlist[i:i+context_length])
+        if context not in context_dict:
+            context_dict[context] = [wordlist[i + context_length]]
+        else:
+            context_dict[context].append(wordlist[i + context_length])
+    return context_dict
 
-def generate_song(context_to_words: dict[tuple[str], List[str]] , num_words: int) -> str:
-    pass
+# def generate_song(context_to_words: dict[tuple[str], list[str]] , num_words: int) -> str:
+#     pass
+
+# lst = ["Hey", "my", "life", "is", "niko", "and", "my", "life", "is", "sick"]
+# print (build_context_to_next_words(lst, 2))
