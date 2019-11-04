3f09513d3add3c0f9db1b52a9e6f6f660b41496a
Nikola Marunic <nikola.marunic@mail.utoronto.ca>
2019-11-04 16:19:14 -0500

    Finished implementing the song generation. More or less works with a single sample of lyrics.

--- build_songs.py
+++ build_songs.py
@@ -29,13 +29,14 @@ def generate_random_song(lyrics: str, context_length: int, num_words:int) -> str
     song = ''
     wordlist = lyrics.split(' ')
     context_to_words = build_context_to_next_words(wordlist, context_length)
-    #print(context_to_words)
-    current_context = wordlist[1: 1 +context_length]
-#    song = generate_song(context_to_words, num_words)
+    print(context_to_words)
+    current_context = wordlist[0:context_length]
 
     for i in range(0, num_words - context_length):
         new_word = get_new_word(context_to_words, tuple(current_context))
-
+        song += (new_word + " ")
+        current_context = current_context[1:] + [new_word]
+    print(song)
     return song
 
 def get_new_word(model: dict, fragment: tuple) -> str:
@@ -45,20 +46,54 @@ def get_new_word(model: dict, fragment: tuple) -> str:
     #print(words)
     return choice(words)
 
-lyrics = "Hey my life is niko and my life is sick"
-generate_random_song(lyrics, 2, 10)
+# lyrics = "Hey my life is niko and my life is sick "
+lyrics = "Truth has to be told, I don't bend, I don't fold, I lost count how many we sold \n " \
+         "I went Silver, I went Gold \n " \
+         "Then I went Platinum, so what's next? \n " \
+         "Supermodels tryna sex \n " \
+         "Sendin' nudes on a text \n " \
+         "Can't believe I used to be vexed \n " \
+         "See, it's too easy \n " \
+         "To write a sad song about how my dad raised me \n " \
+         "'Cause I'm lookin' in the mirror and my dad made me \n " \
+         "A real top boy, I just can't play the victim \n " \
+         "Been livin' my life as a kingpin \n " \
+         "I'm calm with the heat in the kitchen \n " \
+         "I was a young boy, mum told me what my name really means \n " \
+         "And the power just kicked in \n " \
+         "I found my way home \n " \
+         "Then I saw my granddad's name on a gravestone \n " \
+         "The same as mine, already dead \n " \
+         "Nothin' to fear, I been here from time \n " \
+         "Chief SK sippin' on palm wine \n " \
+         "Every day, I laugh at these niggas online \n " \
+         "Another one, here today, gone tomorrow \n " \
+         "Dick ridin; for some likes and a follow \n " \
+         "Put in the work, that's all you need to bust \n " \
+         "Shoutout Lancey, Headie, and J Hus \n " \
+         "Shout 67, oh you see them with us \n " \
+         "We was on tour, bare weed on the bus \n " \
+         "Feds outside, the bus gotta push to the next city, gotta rush \n " \
+         "Big plans gettin' discussed, so freedom is a must \n " \
+         "Fuck the police, tell 'em, Eat my dust \n " \
+        "'Cause still it ain't safe, not even in a world full of cops \n " \
+        "I got bored of askin', When is this hurt gonna stop? \n " \
+        "We don't want to conversate or confer with the opps \n " \
+        "It is what it is \n " \
+        "Recently, I've been learnin' a lot \n " \
+        "All I know is there's no better feeling \n " \
+        "Than gettin' home and seein' my little girl in a cot, so \n " \
+        "This year we're done talkin' \n" \
+        "Forget the bagga chat, it's just action \n " \
+        "Man are trollin' to get a reaction \n " \
+        "Every day, it's another distraction \n " \
+        "Gotta fight temptation, can't get lost in the sauce \n " \
+        "Have I got a heart? Yeah, of course \n " \
+        "But I had to put my feelings on pause \n "
+generate_random_song(lyrics, 1, 100)
+
+
 # lst = ["Hey", "my", "life", "is", "niko", "and", "my", "life", "is", "sick"]
 # print (build_context_to_next_words(lst, 2))
-
-
-# def generate_song(model: dict , num_words: int) -> str:
-#     new_song = ""
-#     current_context = ""
-#     for i in range (num_words):
-#
-#
-#     return new_song
-#
-
 
 
